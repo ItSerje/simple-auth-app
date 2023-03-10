225b0844355c38af90a273c94f3956beee0768b2
ItSerje <66359825+ItSerje@users.noreply.github.com>
2023-03-10 06:46:36 +0600

    add constants

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const token =
+  'patsZk6NAmMEGntD0.d86389b2e889c0c4280a158b0bf4c771955c7aff52ccc0d8080dd0362f936128';
+export const baseUrl = 'https://api.airtable.com/v0/appn3zTP5Hw22ygzT';
